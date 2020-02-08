81a44d2cd0dfecf9e03c2f322f9d9a26f8985a77
rhc822 <rhc822@gmail.com>
2020-02-08 15:30:58 -0600

    Completed chapter 3, app working

---
+++ libraryapp/views/libraries/list.py
@@ -0,0 +1,37 @@
+import sqlite3
+from django.shortcuts import render
+from libraryapp.models import Library
+from ..connection import Connection
+
+
+def library_list(request):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        select
+            l.libraryId,
+            l.library_name,
+            l.library_address,
+        from libraryapp_library l
+        """)
+
+        all_libraries = []
+        dataset = db_cursor.fetchall()
+
+        for row in dataset:
+            library = Library()
+            library.id = row["id"]
+            library.name = row["name"]
+            library.address = row["address"]
+
+            all_libraries.append(lib)
+
+    template_name = 'library/list.html'
+
+    context = {
+        'all_libraries': all_libraries
+    }
+
+    return render(request, template_name, context)
